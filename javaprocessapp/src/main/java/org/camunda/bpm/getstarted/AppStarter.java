b0e8d78305f09fb4fa761e2fc79a0827d1ee8cd3
teyyub <teyyub392@gmail.com>
2020-11-06 12:59:46 +0400

    java servlet applacation starter ucun

---
+++ javaprocessapp/src/main/java/org/camunda/bpm/getstarted/AppStarter.java
@@ -0,0 +1,8 @@
+package org.camunda.bpm.getstarted;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("App")
+public class AppStarter extends ServletProcessApplication {
+}
