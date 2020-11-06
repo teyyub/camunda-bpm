aa0a3a7d8d886e6bda6cd3ffde1dbee1dd71e93c
teyyub <teyyub392@gmail.com>
2020-11-06 13:28:31 +0400

    link de gosterilen video daki kimi her seyi test olundu isleyir

---
+++ javaprocessapp/src/main/java/org/camunda/bpm/getstarted/PrintDelegate.java
@@ -0,0 +1,11 @@
+package org.camunda.bpm.getstarted;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class PrintDelegate implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        System.out.println("processDefinition " +delegateExecution.getProcessDefinitionId()+", activity " + delegateExecution.getCurrentActivityId());
+    }
+}
